d5594e489d053e0bd4e7a4d986a7f0684a1d9c3d
ShotaAk <s.aoki@rt-net.jp>
2022-03-09 11:21:34 +0900

    Cube IDE 1.9.0に対応（互換性も保つはず）

--- build-docker-image-gui.sh
+++ build-docker-image-gui.sh
@@ -3,6 +3,16 @@ set -eu
 
 SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
+# build docker images
+cd ${SRC_DIR}/base
+# インストーラ（zipファイル）名を取得
+FILE_NAME=$(basename installer/*.zip .zip)
+if [ "$FILE_NAME" == "" ]; then
+  echo "ファイルが存在しません"
+  exit 0
+fi
+VERSION=${FILE_NAME: -5}
+
 # get parameter from system
 DOCKER_USER=${USER:-$(id -un)}
 DOCKER_GROUP=`id -gn`
@@ -16,4 +26,5 @@ docker build -t ${DOCKER_USER}/stm32cubeide-gui \
     --build-arg UID=${DOCKER_UID} \
     --build-arg GROUP=${DOCKER_GROUP} \
     --build-arg GID=${DOCKER_GID} \
+    --build-arg VERSION=${VERSION} \
     -f Dockerfile .
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,4 +5,12 @@ SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 # build docker images
 cd ${SRC_DIR}/base
-docker build -t tiryoh/stm32cubeide:1.5.1 -f Dockerfile .
+# インストーラ（zipファイル）名を取得
+FILE_NAME=$(basename installer/*.zip .zip)
+if [ "$FILE_NAME" == "" ]; then
+  echo "ファイルが存在しません"
+  exit 0
+fi
+
+VERSION=${FILE_NAME: -5}
+docker build -t tiryoh/stm32cubeide:$VERSION -f Dockerfile . --build-arg ZIP_FILE_NAME=$FILE_NAME --build-arg VERSION=$VERSION
